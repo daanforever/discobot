babf67654865d4a494f9a43cae5f8851796dae2a
Dan <daan.forever@gmail.com>
2016-07-12 12:55:56 +0300

    disqus added

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -3,5 +3,7 @@ Rails.application.config.after_initialize do
   Bot::Token      = 'MjAxODAzNDA1MTkzMjQ4Nzcw.CmTyAw.9nAGnoosON3WVTRr57BRDIPY1RU'
   Bot::ID         = 201803405193248770
   Bot::Pemission  = 3072
+  Bot::AddLink    = "https://discordapp.com/oauth2/authorize?client_id=#{Bot::AppID}&scope=bot&permissions=#{Bot::Pemission}"
+  
   Bot.connection  if defined?(Rails::Server)
 end
